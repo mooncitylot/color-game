a7e1086ec4e02e0659f71af45bc35fd25b21f53f
Tyler <mooncitylot@gmail.com>
2024-02-07 09:01:34 -0600

    Add time remaining countdown to dashboard

--- src/views/dashboard/dashboard-container.js
+++ src/views/dashboard/dashboard-container.js
@@ -3,10 +3,13 @@ import BackArrowElement from '../../shared/back-arrow.js'
 import { go } from '../../router/router-base.js'
 import routes from '../../router/routes.js'
 import { getGoalColorName } from '../../utility/color-db.js'
+
 class DashboardContainerElement extends LitElement {
   constructor() {
     super()
     this.color = getGoalColorName()
+    this.timeRemaining = this.calculateTimeRemaining()
+    this.startCountdown()
   }
 
   /** @param {RouteEnterArgs} arg0 */
@@ -14,12 +17,36 @@ class DashboardContainerElement extends LitElement {
     console.log('DashboardContainerElement.routeEnter', { nextView, context })
   }
 
+  calculateTimeRemaining() {
+    const now = new Date()
+    const midnight = new Date(now)
+    midnight.setHours(24, 0, 0, 0)
+    return midnight.getTime() - now.getTime()
+  }
+
+  startCountdown() {
+    setInterval(() => {
+      this.timeRemaining = this.calculateTimeRemaining()
+      this.requestUpdate()
+    }, 1000)
+  }
+
+  /**
+   * @param {number} milliseconds
+   */
+  formatTime(milliseconds) {
+    const seconds = Math.floor((milliseconds / 1000) % 60)
+    const minutes = Math.floor((milliseconds / (1000 * 60)) % 60)
+    const hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24)
+    return `${hours.toString().padStart(2, '0')} hours  ${minutes.toString().padStart(2, '0')} minutes`
+  }
+
   render() {
     return html`
       <div class="wrapper">
         <div class="stats">
           <h2>Color of the Day: <span>"${this.color}"</span></h2>
-          <h3>Time Remaining: <span>00:00:00</span></h3>
+          <h3>Time Remaining: <span>${this.formatTime(this.timeRemaining)}</span></h3>
           <h3>Today's Attempts: <span>0</span></h3>
         </div>
         <button class="dashboard-option" @click=${() => go(routes.COLOR_SCAN.path)}>Color Grabber</button>
@@ -54,5 +81,6 @@ class DashboardContainerElement extends LitElement {
     }
   `
 }
+
 customElements.define('dashboard-container', DashboardContainerElement)
 export default DashboardContainerElement
