754de3668aa87f4298b649fa9c6575011cfe9733
Ty Mounsithiraj <55509054+mooncitylot@users.noreply.github.com>
2024-02-16 17:37:32 -0600

    Update dependencies and fix code formatting

--- src/app-enter.js
+++ src/app-enter.js
@@ -1,6 +1,29 @@
 import { html, css } from 'lit'
 import RouterBase from './router/router-base.js'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPmIDCJc_QJz4g_WL7Aou0RMy9gbcF-MU',
+  authDomain: 'color-game-16519.firebaseapp.com',
+  databaseURL: 'https://color-game-16519-default-rtdb.firebaseio.com',
+  projectId: 'color-game-16519',
+  storageBucket: 'color-game-16519.appspot.com',
+  messagingSenderId: '719115016654',
+  appId: '1:719115016654:web:7f17ee630af7b1ffd2339f',
+  measurementId: 'G-88W618NSMR',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
 class AppEnterElement extends RouterBase {
   render() {
     return html` <slot></slot> `
