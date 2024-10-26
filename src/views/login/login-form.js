bc46cfa45d3cb8fef6177465edf938e4f3ae8cc3
Ty Mounsithiraj <55509054+mooncitylot@users.noreply.github.com>
2024-10-25 19:21:32 -0500

    password reset link

--- src/views/login/login-form.js
+++ src/views/login/login-form.js
@@ -2,10 +2,13 @@
 import { LitElement, html, css } from 'lit'
 
 import { setCurrentUser, signIn } from '../../utility/auth-service.js'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
 
 import { routes } from '../../router/routes.js'
 import { go } from '../../router/router-base.js'
 import globalStyles from '../../styles/global-styles.js'
+
+import { auth } from '../../app-enter.js'
 
 import { getUserData } from '../../utility/firebase-utils.js'
 
@@ -14,10 +17,12 @@ import { updateCurrentUser } from '../../utility/auth-service.js'
 class LoginFormElement extends LitElement {
   static properties = {
     email: { type: String },
+    forgotPassword: { type: Boolean },
   }
   constructor() {
     super()
     this.email = ''
+    this.forgotPassword = false
   }
 
   /** @param {Event & {target: HTMLFormElement}} e */
@@ -47,50 +52,63 @@ class LoginFormElement extends LitElement {
       })
   }
 
-  handleForgotPassword(e) {
+  async handleForgotPassword(e) {
     e.preventDefault()
-    go(routes.FORGOT_PASSWORD.path)
+    const email = this.shadowRoot.querySelector('input[name="forgotEmail"]').value
+    try {
+      await sendPasswordResetEmail(auth, email)
+      alert('Password reset email sent. Please check your inbox.')
+    } catch (error) {
+      console.error('Password reset failed:', error)
+      alert('Failed to send password reset email. Please try again.')
+    }
   }
 
   render() {
-    return html`
-      <form
-        slot="body"
-        autocomplete="on"
-        @submit=${this.handleFormSubmit}
-        @keypress=${(/** @type {*} */ e) => {
-          if (e.key === 'Enter')
-            // @ts-ignore
-            this.shadowRoot.querySelector('#submit-button').click()
-        }}
-      >
-        <input
-          autocomplete="on"
-          @invalid="this.setCustomValidity('Please enter your email')"
-          type="email"
-          value=${this.email}
-          name="email"
-          placeholder="Email"
-          required
-        />
-        <input
-          autocomplete="on"
-          @invalid="this.setCustomValidity('Please enter a valid password')"
-          type="password"
-          name="password"
-          placeholder="Password"
-          required
-        />
-        <div class="button-wrap">
-          <button class="login-option-1" type="submit" id="submit-button">Log In</button>
-          <div class="separator-bar">
-            <span class="separator-text">OR</span>
-          </div>
-          <button class="login-option-1" @click=${() => go(routes.SIGNUP.path)}>Sign Up</button>
-        </div>
-      </form>
-      <div class="background"></div>
-    `
+    return this.forgotPassword
+      ? html`<form @submit=${this.handleForgotPassword}>
+          <input type="email" name="forgotEmail" required placeholder="Enter your email" />
+          <button type="submit">Send Reset Email</button>
+          <a @click=${() => (this.forgotPassword = false)}>Back to Login</a>
+        </form> `
+      : html`<form
+            slot="body"
+            autocomplete="on"
+            @submit=${this.handleFormSubmit}
+            @keypress=${(/** @type {*} */ e) => {
+              if (e.key === 'Enter')
+                // @ts-ignore
+                this.shadowRoot.querySelector('#submit-button').click()
+            }}
+          >
+            <input
+              autocomplete="on"
+              @invalid="this.setCustomValidity('Please enter your email')"
+              type="email"
+              value=${this.email}
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              autocomplete="on"
+              @invalid="this.setCustomValidity('Please enter a valid password')"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <div class="button-wrap">
+              <button class="login-option-1" type="submit" id="submit-button">Log In</button>
+              <div class="separator-bar">
+                <span class="separator-text">OR</span>
+              </div>
+              <button class="login-option-1" @click=${() => go(routes.SIGNUP.path)}>Sign Up</button>
+            </div>
+            <a @click=${() => (this.forgotPassword = true)}>Forgot Password?</a>
+          </form>
+          <div class="background"></div>`
+    return html``
   }
 
   static styles = [
